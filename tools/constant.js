16f1c64bf25b1671d4216cbca65c1404c3ec602d
史宁 <ShinIng@shiningdeMacBook-Pro.local>
2016-05-19 17:51:10 +0800

    scan car number

--- tools/constant.js
+++ tools/constant.js
@@ -9,7 +9,10 @@ var constant = {
 	'PASSWORD_ERROR'  : '密码错误',
 	'PAGE_SIZE' : 10,
 	'UPLOAD_SUCCESS' : '上传成功',
-	'QINIU_PATH' : 'http://7xu64v.com1.z0.glb.clouddn.com/'
+	'QINIU_PATH' : 'http://7xu64v.com1.z0.glb.clouddn.com/',
+	'SB_PATH'  : 'http://netocr.com/api/recog.do',
+	'SB_KEY'   : 'PJGje1KFTMPJuqp4kpAGVa',
+	'SB_SECRET': '071929290085490aac4ceccd92bc3b39'
 };
 
 module.exports = constant;
---
+++ tools/scan_car.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var constant = require('./constant');
+var sql = require('../dao/sql_tool');
+var sql_mapping = require('../dao/sql_mapping');
+var request = require('request');
+var fs = require('fs');
+
+
+exports.scan_car = function(req, res, next, result, uid, pic_url, time, adcode, loc, tag, text, fav, filePath, fileSize) {
+	var r = request.post(constant.SB_PATH, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var body = JSON.parse(response.body);
+			if (body.message.status == 0){
+				var car_num = body.cardsinfo[0].items[0].content;
+			} else {
+				var car_num = '';
+			}
+			var values = [uid, pic_url, time, adcode, loc, car_num, '', '', 0, 0, tag, text, fav];
+			sql.query(req, res, sql_mapping.upload_img, values, next, function(err, ret){
+				result.header.code = '200';
+				result.header.msg  = constant.SUCCESS;
+				result.data        = {result : '0',
+									  msg    : constant.UPLOAD_SUCCESS};
+				res.json(result);
+			});
+		}
+		});
+	var form = r.form();
+	form.append('typeId', 19);
+	form.append('key', constant.SB_KEY);
+	form.append('secret', constant.SB_SECRET);
+	form.append('format', 'json');
+	form.append('file', fs.createReadStream(filePath));
+}
